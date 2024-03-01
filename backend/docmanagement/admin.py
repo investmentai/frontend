1486fab6ef5ee4796d3cfee9aede907d12e612e0
Hasan Satasmaz <hsatasmaz@gmail.com>
2024-03-01 23:29:46 +0300

    Backend removed

--- backend/docmanagement/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserApiKey, LLMProvider, UserDocument, UserFolder
-
-admin.site.register(UserApiKey)
-admin.site.register(LLMProvider)
-admin.site.register(UserDocument)
-admin.site.register(UserFolder)
--- backend/docmanagement/llm/redis.py
+++
@@ -1,7 +0,0 @@
-# import redis
-
-# r = redis.Redis(
-#     host="redis-14656.c78.eu-west-1-2.ec2.cloud.redislabs.com",
-#     port=14656,
-#     password="k6Ljb6HtapVBJpXq37YoSIH0Q5kmLRhM",
-# )
